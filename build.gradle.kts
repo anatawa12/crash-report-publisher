4d053c1290ad79c9596bf4b38a9b50dc2bbadbea
anatawa12 <anatawa12@icloud.com>
2021-04-09 23:09:25 +0900

    initial commit of crash report publisher 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.3.72"
+    kotlin("jvm") version "1.4.32"
     id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
@@ -13,7 +13,10 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("org.apache.httpcomponents:httpclient:4.5.13")
+    implementation("org.apache.httpcomponents:httpmime:4.5.13")
     shadow("net.minecraft:launchwrapper:1.12")
+    shadow("org.ow2.asm:asm:5.0")
 }
 
 tasks {
@@ -38,5 +41,13 @@ tasks {
         archiveClassifier.set("")
         val basePkg = "com.anatawa12.crashReportPublisher.libs"
         relocate("kotlin.", "$basePkg.kotlin.")
+        relocate("org.apache.http.", "$basePkg.apache.http.")
+        relocate("org.apache.commons.codec.", "$basePkg.apache.commons.codec.")
+        relocate("org.apache.commons.logging.", "$basePkg.apache.commons.logging.")
+        relocate("org.apache.commons.codec.", "$basePkg.apache.commons.codec.")
+        relocate("org.intellij.lang.annotations.", "$basePkg.ij_annotations.")
+        relocate("org.jetbrains.annotations.", "$basePkg.jb_annotations.")
+
+        exclude("META-INF/**")
     }
 }
